313e001edf66152113b9ceeef577e2a067f931d7
Arnold, Stephen C <STEPHEN_C_ARNOLD@HOMEDEPOT.COM>
2025-01-14 15:36:56 -0500

    cleanup and gitignore update

--- setup_lab.sh
+++ setup_lab.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
 # Create directories for the observability project
-mkdir -p grafana_data
-mkdir -p grafana_provisioning
-mkdir -p terraform_data
-
-echo "Created directories:"
-echo "- grafana_data"
-echo "- grafana_provisioning"
-echo "- terraform_data"
+mkdir -p grafana-data
+mkdir -p grafana-provisioning
+mkdir -p terraform-data
+mkdir -p jenkins-data
 
 # Start Docker containers in detached mode
 docker compose up -d
