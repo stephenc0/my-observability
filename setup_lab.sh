af174c0e01ed25ab1bf24231653a39f77ad99857
Arnold, Stephen C <STEPHEN_C_ARNOLD@HOMEDEPOT.COM>
2025-01-14 15:23:57 -0500

    updated lab setup script

--- setup_lab.sh
+++ setup_lab.sh
@@ -14,3 +14,13 @@ echo "- terraform_data"
 docker compose up -d
 
 echo "Docker containers started in detached mode"
+
+# Make scripts executable
+chmod +x reset_prom_data.sh
+chmod +x update_alerts.sh
+chmod +x wipe_lab.sh
+
+echo "Made scripts executable:"
+echo "- reset_prom_data.sh"
+echo "- update_alerts.sh"
+echo "- wipe_lab.sh"
